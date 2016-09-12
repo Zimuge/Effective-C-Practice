63805b7fcb164f4ec70a19d4db7a82cd2926ff8a
Topon <Topon@DESKTOP-H5M1M8F>
2016-09-12 22:56:03 +0800

    在熟悉一遍

--- TMP/TMP/TMP.cpp
+++ TMP/TMP/TMP.cpp
@@ -15,10 +15,24 @@ int factorial<1>()
 }
 
 
+template<int N>
+int recursion()
+{
+	return N + recursion<N - 1 >();
+}
+
+template<>
+int recursion<1>()
+{
+	return 1;
+}
+
 int main()
 {
 	int n = factorial<5>();
+	int sum = recursion<100>();
 	std::cout << n << std::endl;
+	std::cout << sum << std::endl;
     return 0;
 }
 
